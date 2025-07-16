9e561d25a05e2fcb5f892099fe1c5d40129d6d91
Peter <pp771007@gmail.com>
2025-07-16 18:21:18 +0800

    feat: 移除 Service Worker 中的快取邏輯，簡化 PWA 安裝流程

--- static/sw.js
+++ static/sw.js
@@ -1,30 +1,4 @@
-const CACHE_NAME = 'maccms-parser-cache-v1';
-const urlsToCache = [
-    '/',
-    '/static/css/style.css',
-    '/static/js/main.js',
-    '/static/img/favicon.svg'
-];
-
-self.addEventListener('install', event => {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(cache => {
-                console.log('Opened cache');
-                return cache.addAll(urlsToCache);
-            })
-    );
-});
-
-self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request)
-            .then(response => {
-                if (response) {
-                    return response;
-                }
-                return fetch(event.request);
-            }
-            )
-    );
+self.addEventListener('fetch', () => {
+    // This empty fetch handler is required to make the app installable as a PWA.
+    // It doesn't cache anything, allowing the browser to handle requests normally.
 });
